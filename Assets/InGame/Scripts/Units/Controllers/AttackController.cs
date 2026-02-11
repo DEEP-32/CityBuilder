99b2c9d2d9fb80d89efa43861edb9af0493c393a
Deepanshu Singh <anshu3012ep@gmail.com>
2026-02-11 14:15:07 +0530

    [ADD] Attack controller for units

---
+++ Assets/InGame/Scripts/Units/Controllers/AttackController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace BasicRTS.Units.Controllers {
+    
+    public class AttackController : MonoBehaviour {
+
+        [SerializeField] Transform targetToAttack;
+
+        void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Enemy") && targetToAttack == null) {
+                targetToAttack =  other.transform;
+            }
+        }
+
+        void OnTriggerExit(Collider other) {
+            if (other.CompareTag("Enemy") && targetToAttack != null) {
+                targetToAttack = null;
+            }
+        }
+    }
+}
